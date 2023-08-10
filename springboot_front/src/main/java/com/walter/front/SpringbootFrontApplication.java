cda140101c237c3ffb6680d06ad4350297a36194
walter.hwang <lyvius2@naver.com>
2023-08-10 22:53:30 +0900

    프런트 애플리케이션 프로젝트 추가

---
+++ springboot_front/src/main/java/com/walter/front/SpringbootFrontApplication.java
@@ -0,0 +1,13 @@
+package com.walter.front;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFrontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFrontApplication.class, args);
+	}
+
+}
---
+++ springboot_front/src/test/java/com/walter/front/SpringbootFrontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.walter.front;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFrontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
